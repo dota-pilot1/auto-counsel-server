cbf02df25663ee3d0f96d773f28f6f54be8edd3c
oh hyun seok <hyunsokstar@gmail.com>
2026-01-07 15:53:54 +0900

    first commit

--- src/main/java/io/opro/autocounsel/application/service/CounselService.java
+++ src/main/java/io/opro/autocounsel/application/service/CounselService.java
@@ -1,16 +1,16 @@
 package io.opro.autocounsel.application.service;
 
 import io.opro.autocounsel.domain.model.CounselMessage;
-import io.opro.autocounsel.infrastructure.persistence.mybatis.CounselMapper;
+import io.opro.autocounsel.domain.repository.CounselRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class CounselService {
-    private final CounselMapper counselMapper;
+    private final CounselRepository counselRepository;
 
     public CounselMessage getHello() {
-        return counselMapper.getHelloMessage();
+        return counselRepository.getHelloMessage();
     }
 }
--- src/main/java/io/opro/autocounsel/infrastructure/persistence/mybatis/CounselMapper.java
+++ src/main/java/io/opro/autocounsel/infrastructure/persistence/mybatis/CounselMapper.java
@@ -1,11 +1,13 @@
 package io.opro.autocounsel.infrastructure.persistence.mybatis;
 
 import io.opro.autocounsel.domain.model.CounselMessage;
+import io.opro.autocounsel.domain.repository.CounselRepository;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
-public interface CounselMapper {
-    @Select("SELECT 'Hello World from Database!' as message")
+public interface CounselMapper extends CounselRepository {
+    @Override
+    @Select("SELECT message FROM counsels LIMIT 1")
     CounselMessage getHelloMessage();
 }
